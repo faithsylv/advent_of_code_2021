9f731525b0d7238f2926e75c7d1c171f72b1041f
faithsylv <faith.sylvia@envato.com>
2021-12-10 11:26:48 +1100

    Clean up tweaks to D1

--- lib/day1.rb
+++ lib/day1.rb
@@ -1,5 +1,3 @@
-puts 'hello'
-
 # input = %(199
 # 200
 # 208
@@ -11,11 +9,11 @@ puts 'hello'
 # 260
 # 263)
 
-input = File.read('day1.txt')
+input = File.read('inputs/day1.txt')
 
 input_array = input.split("\n").map( &:to_i )
 
 # puts input_array.each_cons(2).select{|(a,b)| b > a}.size
 
-puts input_array.each_cons(3).map{|array| array.sum }.each_cons(2).select{|(a,b)| b > a}.size
+puts input_array.each_cons(3).map(&:sum).each_cons(2).select{|(a,b)| b > a}.size
 # if you want to print each tuple --> each {|item| puts item.to_s + "\n"}
